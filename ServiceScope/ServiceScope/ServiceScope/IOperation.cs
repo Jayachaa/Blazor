9c043227cb51bc4771633374517db2ae6c9b8fe4
JayachandranJaganath <mailforjaya84@gmail.com>
2023-04-26 17:42:13 +0530

    ADDED: Project is added - Servicescope

---
+++ ServiceScope/ServiceScope/ServiceScope/IOperation.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceScope;
+
+public interface IOperation
+{
+	string OperationId { get; }
+}
+
+public interface IOperationTransient : IOperation
+{
+}
+
+public interface IOperationScoped : IOperation
+{
+}
+
+public interface IOperationSingleton : IOperation
+{
+}
---
+++ ServiceScope/ServiceScope/ServiceScope/Operation.cs
@@ -0,0 +1,16 @@
+﻿namespace ServiceScope
+{
+	public class Operation : IOperationTransient, IOperationScoped, IOperationSingleton
+	{
+		#region Properties
+		public string OperationId { get; }
+		#endregion
+
+		#region Constructors
+		public Operation()
+		{
+			this.OperationId = Guid.NewGuid().ToString()[^4..];
+		}
+		#endregion
+	}
+}
